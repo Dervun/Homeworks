50261c584341a0312e2d8ade55407cc838cc22e9
Dervun <masharskyss@gmail.com>
2015-12-17 19:19:41 +0300

    I added incorrect file (homework 2 instead homework 3) Now it's clear.

--- 1/homework 1/task_1_3/main.cpp
+++ 1/homework 1/task_1_3/main.cpp
@@ -1,46 +1,37 @@
 /*!
- * > Реализовать алгоритм нахождения неполного частного от деления a на b (целые числа), используя только операции сложения, вычитания и умножения.
+ * > Дан массив целых чисел x[1]...x[m+n], рассматриваемый как соединение двух его отрезков: начала x[1]...x[m] длины m и конца x[m+1]...x[m+n] длины n.
+ * > Не используя дополнительных массивов, переставить начало и конец.
  */
 #include <stdio.h>
-#include <cstdlib>
+#include <algorithm>
+
+using namespace std;
+
+void turn(int *arr, int left, int right)
+{
+    for (int j = left; j < left + (right - left) / 2; j++)
+        swap(arr[j], arr[right + left - j]);
+}
+
 main()
 {
-    int a = 0;
-    int b = 0;
-    printf("Enter integer a/b:\n");
-    scanf("%d/%d", &a, &b);
-    int result = 0;
-    if (b == 0)
-    {
-        printf("Math error. Enter b not equal 0.");
-        exit(1);
-    }
-    if ((a >= 0 && b > 0) || (a < 0 && b < 0))
-    {
-        if (abs(a) < abs(b))
-            result = 0;
-        else
-        {
-            while (abs(a) >= abs(b))
-            {
-                a -= b;
-                result += 1;
-            }
-        }
-    }
-    else
-    {
-        if (abs(a) < abs(b))
-            result = 0;
-        else
-        {
-            while (abs(a) >= abs(b))
-            {
-                a += b;
-                result += 1;
-            }
-            result *= (-1);
-        }
-    }
-    printf("Result:\n %d\n", result);
+    printf("Enter m, n (e.g. '5, 8'):\n");
+    int m = 0;
+    int n = 0;
+    scanf("%d, %d", &m, &n);
+    int arraySize = m + n;
+    int array[arraySize];
+    for (int i = 0; i < arraySize; i++)
+        array[i] = i;
+    printf("Before:\n");
+    for (int i = 0; i < arraySize - 1; i++)
+        printf("%d:", array[i]);
+    printf("%d\n", array[arraySize - 1]);
+    turn(array, 0, m - 1);
+    turn(array, m, arraySize - 1);
+    turn(array, 0, arraySize - 1);
+    printf("After:\n");
+    for (int i = 0; i < arraySize - 1; i++)
+        printf("%d:", array[i]);
+    printf("%d\n", array[m+n-1]);
 }
