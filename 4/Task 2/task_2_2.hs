ef13f9db99ca5b4c42c50aff1d75257c3cc99dae
Dervun <masharskyss@gmail.com>
2017-03-10 02:44:16 +0300

    Rewrited with map function

--- 4/Task 2/task_2_2.hs
+++ 4/Task 2/task_2_2.hs
@@ -2,8 +2,4 @@ module Two (twoPow) where
 
 twoPow :: Integer -> [Integer]
 twoPow n | n < 1      = error "expected argument >= 1"
-         | otherwise  = for [] 2 n
-    where
-        for :: [Integer] -> Integer -> Integer -> [Integer]
-        for currentList currentValue i | i == 0     = currentList
-                                       | otherwise  = for (currentList ++ [currentValue]) (currentValue * 2) (i - 1)
+         | otherwise  = map (2^) [1..n]
